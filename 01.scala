2fd61e5c8c97f4e2f7308d935a849d52ed19fb80
Christian Zambrano <czambran@gmail.com>
2014-02-21 23:12:48 -0500

    Adding answers for exercises 3.3 and 3.4

--- 01.scala
+++ 01.scala
@@ -23,6 +23,37 @@ object Chapter3 extends App {
 	
 	
 	println("Exercise 3.3")
+	val array_with_swapped_values = for (i <- 0 until original_array.length) 
+	  yield (
+	      if (i == original_array.length - 1)
+	        original_array(i)
+	      else if (i % 2 == 0)
+	        original_array(i+1)
+	      else
+	        original_array(i-1)
+	      )
+	
+	println(array_with_swapped_values.toArray.mkString(","))
+	println()
+	
+	println("Exercise 3.4")
+	import collection.mutable.ArrayBuffer
+	val exercise_3_4_original_array = Array(2,3,4,0,-2,4,8,0,10,-1)
+	val sorted_array_buffer = new ArrayBuffer[Int]()
+	// Expected output (2,3,4,4,8,10, 0,-2,0,-1)
+	var positive_values = new ArrayBuffer[Int]()
+	var zero_and_negative_values = new ArrayBuffer[Int]()
+	for (i <- 0 until exercise_3_4_original_array.length) {
+		if (exercise_3_4_original_array(i) > 0)
+		  positive_values += exercise_3_4_original_array(i)
+		else
+		  zero_and_negative_values += exercise_3_4_original_array(i)
+	}
+	sorted_array_buffer ++= positive_values
+	sorted_array_buffer ++= zero_and_negative_values
+	println(sorted_array_buffer.toArray.mkString(","))
+	println()
+	
 	
 	
 }
